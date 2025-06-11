0497c2aa981cdc13a76ffd1e6ddf07407d344402
Alexis2.0 <asanchezrod@ucvvirtual.edu.pe>
2025-06-10 20:56:01 -0500

    Create user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { CreateUserDto } from './entities/dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<{ message: string }> {
+    const hashedPassword = await bcrypt.hash(createUserDto.contrasena, 10);
+    const user = this.userRepository.create({
+      ...createUserDto,
+      contrasena: hashedPassword,
+    });
+    await this.userRepository.save(user);
+    return { message: 'Usuario creado correctamente' };
+  }
+}
