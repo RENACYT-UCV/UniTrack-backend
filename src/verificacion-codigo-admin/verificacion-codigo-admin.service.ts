4e6b0f47b8a62992d1b8bb2868c276f13dac0b16
Cristhian Caballero Moscoso <166889169+Cris-CM@users.noreply.github.com>
2025-06-14 01:14:34 -0500

    feat: rellenado funciones vacias

--- src/verificacion-codigo-admin/verificacion-codigo-admin.service.ts
+++ src/verificacion-codigo-admin/verificacion-codigo-admin.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { CreateVerificacionCodigoAdminDto } from './dto/createVerificacion-codigo-admin.dto';
+import { UpdateVerificacionCodigoAdminDto } from './dto/updateVerificacion-codigo-admin.dto';
 
 @Injectable()
-export class VerificacionCodigoAdminService {}
+export class VerificacionCodigoAdminService {
+    crear(dto: CreateVerificacionCodigoAdminDto) {
+        return 'This action adds a new verificacionCodigoAdmin';
+      }
+
+      actualizar(id: number, dto: UpdateVerificacionCodigoAdminDto) {
+        return 'This action updates a #${id} verificacionCodigoAdmin';
+      }
+
+      eliminar(id: number) {
+        return 'This action removes a #${id} verificacionCodigoAdmin';
+      }
+
+      encontrarPorAdmin(idAdmin: number) {
+        return 'This action returns a #${idAdmin} verificacionCodigoAdmin';
+      }
+}
