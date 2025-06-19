ffeffc5c90e2dd0c8f3adf684d4b3ffc413dee51
Alexis2.0 <asanchezrod@ucvvirtual.edu.pe>
2025-06-18 21:02:23 -0500

    Fix verificarExpiracion

--- src/qr/dto/verificar-qr.dto.ts
+++ src/qr/dto/verificar-qr.dto.ts
@@ -1,12 +1,6 @@
 import { IsNumber, IsString } from 'class-validator';
 
 export class VerificarQrDto {
-  @IsNumber()
-  idUsuario: number;
-
   @IsString()
   hash: string;
-
-  @IsString()
-  tipo: string;
 }
