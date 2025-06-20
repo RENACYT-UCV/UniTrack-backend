d248e7bc1cc6eee916dc544755d09497954d51ab
nramirezhelvis <ramirezherreranoeelvis@gmail.com>
2025-06-20 03:05:01 -0500

    fix: correctin add methos service and controller for  password

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -8,13 +8,20 @@ import { QueryFailedError } from 'typeorm';
 import { LoginAdminDto } from './dto/login-admin.dto';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+import { ResetPasswordDto } from 'src/users/dto/reset-password.dto';
+import { ForgotPasswordDto } from 'src/users/dto/forgot-password.dto';
+import { MailService } from 'src/mail/mail.service';
 
 @Injectable()
 export class AdminService {
+  private passwordResetCodes: Map<string, { code: string; expiry: Date }> =
+    new Map();
+
   constructor(
     @InjectRepository(Admin)
     private adminRepository: Repository<Admin>,
     private jwtService: JwtService,
+    private mailService: MailService,
   ) {}
 
   async create(
@@ -31,9 +38,9 @@ export class AdminService {
       const access_token = await this.jwtService.signAsync(payload, {
         secret: process.env.JWT_SECRET,
       });
-      return { 
+      return {
         message: 'Administrador creado correctamente',
-        access_token 
+        access_token,
       };
     } catch (error) {
       if (
@@ -79,12 +86,16 @@ export class AdminService {
     }
   }
 
-  async login(loginAdminDto: LoginAdminDto): Promise<{ access_token: string, current_user }> {
+  async login(
+    loginAdminDto: LoginAdminDto,
+  ): Promise<{ access_token: string; current_user }> {
     const admin = await this.adminRepository.findOne({
       where: { correo: loginAdminDto.correo },
     });
     if (!admin) {
-      throw new NotFoundException(`Admin with correo ${loginAdminDto.correo} not found`);
+      throw new NotFoundException(
+        `Admin with correo ${loginAdminDto.correo} not found`,
+      );
     }
     const payload = { sub: admin.idAdmin, username: admin.correo };
     return {
@@ -94,5 +105,83 @@ export class AdminService {
         expiresIn: '1h',
       }),
     };
+  }
+
+  async forgotPassword(
+    forgotPasswordDto: ForgotPasswordDto,
+  ): Promise<{ message?: string; error?: string }> {
+    const { correo } = forgotPasswordDto;
+    const user = await this.adminRepository.findOne({ where: { correo } });
+
+    if (!user) {
+      return { error: 'Administrador no encontrado' };
+    }
+
+    const code = Math.floor(100000 + Math.random() * 900000).toString(); // 6-digit code
+    const expiry = new Date(Date.now() + 10 * 60 * 1000); // Code valid for 10 minutes
+    this.passwordResetCodes.set(correo, { code, expiry });
+
+    const subject = 'Restablecimiento de Contraseña';
+    const text = `Su código de restablecimiento de contraseña es: ${code}. Este código es válido por 10 minutos.`;
+    const html = `<p>Su código de restablecimiento de contraseña es: <strong>${code}</strong>. Este código es válido por 10 minutos.</p>`;
+    try {
+      await this.mailService.sendMail(correo, subject, text, html);
+      return {
+        message: `Código de restablecimiento enviado a su correo. ${code}`,
+      };
+    } catch (error) {
+      console.error('Error al enviar correo de restablecimiento:', error);
+      return { error: 'Error al enviar el correo de restablecimiento.' };
+    }
+  }
+
+  async verifyToken(email: string, code: number) {
+    const storedCode = this.passwordResetCodes.get(email);
+    if (!storedCode) {
+      return { error: 'Código no encontrado o expirado.' };
+    }
+
+    if (new Date() > storedCode.expiry) {
+      this.passwordResetCodes.delete(email);
+      return { error: 'Código expirado.' };
+    }
+
+    // compare the provided code with the stored code
+    if (storedCode.code !== code.toString()) {
+      return { error: 'Código inválido.' };
+    }
+    // If the code is valid, return a success message
+    this.passwordResetCodes.delete(email);
+
+    return { message: 'Código verificado correctamente.' };
+  }
+
+  async resetPassword(
+    resetPasswordDto: ResetPasswordDto,
+  ): Promise<{ message?: string; error?: string }> {
+    const { correo, code, newPassword } = resetPasswordDto;
+    const storedCode = this.passwordResetCodes.get(correo);
+
+    if (!storedCode || storedCode.code !== code) {
+      return { error: 'Código inválido o expirado.' };
+    }
+
+    if (new Date() > storedCode.expiry) {
+      this.passwordResetCodes.delete(correo); // Remove expired code
+      return { error: 'Código expirado.' };
+    }
+
+    const admin = await this.adminRepository.findOne({ where: { correo } });
+    if (!admin) {
+      return { error: 'Administrador no encontrado.' };
+    }
+
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    await this.adminRepository.update(admin.idAdmin, {
+      contrasena: hashedPassword,
+    });
+    this.passwordResetCodes.delete(correo); // Invalidate code after successful reset
+
+    return { message: 'Contraseña actualizada correctamente.' };
   }
 }
